fe67691b9fc49fcf716d70e93dab9993240ada39
Justin Greer <justin@justin-greer.com>
2020-11-30 00:20:38 -0500

    Added User Cred Grant Type Example

--- generic/user-grant-type.php
+++ generic/user-grant-type.php
@@ -11,24 +11,22 @@ $redirect_uri = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_S
 $tmp = null;
 
 /**
- * This section is demonstrating the basics of capturing the authorization code returned from WP OAuth Server.
- * It also shows how to use cURL to use the code to retrieve an access token.
+ * This section is demonstrating the basics of capturing the user login informaiton from a form and then posting the
+ * info to WP OAuth Server to get an access token
  */
-if ( isset( $_GET['code'] ) ) {
+if ( isset( $_POST['action'] ) ) {
 
 	$curl_post_data = array(
-		'grant_type'    => 'authorization_code',
-		'code'          => $_GET['code'],
-		'redirect_uri'  => $redirect_uri,
+		'grant_type'    => 'password',
+		'username'      => $_POST['username'],
+		'password'      => $_POST['password'],
 		'client_id'     => $client_id,
 		'client_secret' => $client_secret
 	);
 
-
-	$curl = curl_init( $server_url . '/oauth/token/' );
-
-	//curl_setopt( $curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
-	//curl_setopt( $curl, CURLOPT_USERPWD, $client_id . ':' . $client_secret ); // Your credentials goes here
+	$curl = curl_init( $server_url . '/oauth/token' );
+	//curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	//curl_setopt( $curl, CURLOPT_USERPWD, $client_id . ':' . $client_secret );
 	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
 	curl_setopt( $curl, CURLOPT_POST, true );
 	curl_setopt( $curl, CURLOPT_POSTFIELDS, $curl_post_data );
@@ -36,11 +34,9 @@ if ( isset( $_GET['code'] ) ) {
 	curl_setopt( $curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 GTB5' );
 	curl_setopt( $curl, CURLOPT_REFERER, 'http://www.example.com/1' );
 
-	$curl_response = curl_exec( $curl );
-	$code_response = json_decode( $curl_response );
+	$tmp = json_decode( curl_exec( $curl ) );
 	curl_close( $curl );
 
-	$tmp = $code_response;
 
 	/*
 	 * If there is no error in the return, the following will request the user information from the server
@@ -93,14 +89,15 @@ if ( isset( $_GET['code'] ) ) {
 
     <h3>Login Form Example</h3>
     <p>The form below is a basic example of a simple button to present the user with a login button.</p>
-    <form action="<?php echo $server_url; ?>/oauth/authorize/"
-          method="get">
-        <input type="hidden" name="state" value="abcd123"/>
-        <input type="hidden" name="scope" value="basic"/>
-        <input type="hidden" name="response_type" value="code"/>
+    <form action="" method="post">
+        <input type="hidden" name="action" value="usercredentials"/>
+        <input type="text" name="username" placeholder="username"/><br/>
+        <input type="password" name="password" placeholder="password"/><br/>
+
         <input type="hidden" name="client_id" value="<?php echo $client_id; ?>"/>
-        <input type="hidden" name="redirect_uri" value="<?php echo $redirect_uri; ?>"/>
-        <button type="submit">Log In using Single Sign On</button>
+        <input type="hidden" name="client_secret" value="<?php echo $client_secret; ?>"/>
+
+        <button type="submit">Login</button>
     </form>
 <?php } ?>
 
