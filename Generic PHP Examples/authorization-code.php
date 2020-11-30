cacbe8ddb6aca72fe2325aeff1924d1fc54a8979
Justin Greer <justin@justin-greer.com>
2020-11-29 22:40:26 -0500

    Wipe

---
+++ Generic PHP Examples/authorization-code.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Authorization Code Grant Type - Example</title>
+    <meta name="description" content="Authorization Code Grant Type - Example">
+    <meta name="author" content="WP OAuth Server">
+
+    <link rel="stylesheet" href="assets/styles.css">
+
+</head>
+
+<body>
+<script src="js/scripts.js"></script>
+</body>
+</html>
